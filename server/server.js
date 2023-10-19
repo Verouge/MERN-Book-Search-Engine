cf808358e54b6abbc3c9ff937f8c3582c3c40eca
Verouge <branola1998628@gmail.com>
2023-10-19 20:34:25 +1100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,12 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+// Set up Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
-db.once('open', () => {
+// Apply Apollo GraphQL middleware and set the path to '/graphql'
+server.applyMiddleware({ app, path: "/graphql" });
+
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
